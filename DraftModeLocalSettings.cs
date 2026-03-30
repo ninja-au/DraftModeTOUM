1efa6335bad99a3c2a59c7309393dcf38be6d36e
IdkImNeil <69025896+idkimneil@users.noreply.github.com>
2026-03-30 18:03:56 -0400

    Refactor DraftModeLocalSettings by cleaning up code
    Removed unused using directives and local settings.

--- DraftModeLocalSettings.cs
+++ DraftModeLocalSettings.cs
@@ -1,14 +1,8 @@
-﻿using BepInEx.Configuration;
+using BepInEx.Configuration;
 using MiraAPI.LocalSettings;
-using MiraAPI.LocalSettings.Attributes;
-using MiraAPI.LocalSettings.SettingTypes;
 using TownOfUs.Assets;
 
 namespace DraftModeTOUM;
-
-
-
-
 
 public sealed class DraftModeLocalSettings(ConfigFile config) : LocalSettingsTab(config)
 {
@@ -17,23 +11,6 @@ public sealed class DraftModeLocalSettings(ConfigFile config) : LocalSettingsTab
 
     public override LocalSettingTabAppearance TabAppearance => new()
     {
-        TabIcon = TouAssets.TouMiraIcon
+        TabIcon = TouRoleIcons.Traitor
     };
-
-    
-    
-    
-    
-    [LocalToggleSetting]
-    public ConfigEntry<bool> OverrideUiStyle { get; private set; } =
-        config.Bind("DraftLocal", "OverrideUiStyle", false);
-
-    
-    
-    
-    
-    [LocalToggleSetting]
-    public ConfigEntry<bool> UseCircleStyle { get; private set; } =
-        config.Bind("DraftLocal", "UseCircleStyle", false);
 }
-
