2fe285d987f5564f4917c4f9d2ae50303b8ef9a2
Ninja-AU <notnicotv@gmail.com>
2026-03-30 18:21:33 -0400

    Changed how options show

--- DraftTypeOptions.cs
+++ DraftTypeOptions.cs
@@ -4,20 +4,17 @@ using MiraAPI.Utilities;
 
 namespace DraftModeTOUM;
 
+public enum DraftTypeMode
+{
+    Normal = 0,
+    BanDraft = 1
+}
+
 public sealed class DraftTypeOptions : AbstractOptionGroup
 {
     public override string GroupName => "Draft Type";
     public override uint GroupPriority => 90;
 
-    [ModdedToggleOption("Ban + Draft Mode")]
-    public bool EnableBanDraft { get; set; } = false;
-
-    [ModdedNumberOption("Banned Roles Count", 1f, 10f, 1f, MiraNumberSuffixes.None, "0")]
-    public float BanRoleCount { get; set; } = 3f;
-
-    [ModdedToggleOption("Show Banned Roles")]
-    public bool ShowBannedRoles { get; set; } = true;
-
-    [ModdedToggleOption("Anonymous Ban Users")]
-    public bool AnonymousBanUsers { get; set; } = false;
+    [ModdedEnumOption("Draft Type", typeof(DraftTypeMode), new[] { "Normal Draft", "Ban + Draft" })]
+    public DraftTypeMode DraftType { get; set; } = DraftTypeMode.Normal;
 }
