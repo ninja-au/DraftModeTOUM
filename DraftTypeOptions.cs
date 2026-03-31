c81b83ccab6ad507800df1eeac062d2844d62432
Ninja-AU <notnicotv@gmail.com>
2026-03-31 03:54:57 -0400

    Update DraftTypeOptions.cs

--- DraftTypeOptions.cs
+++ DraftTypeOptions.cs
@@ -7,8 +7,7 @@ namespace DraftModeTOUM;
 public enum DraftTypeMode
 {
     Normal = 0,
-    BanDraft = 1,
-    TeamCaptainBR = 2
+    BanDraft = 1
 }
 
 public sealed class DraftTypeOptions : AbstractOptionGroup
@@ -16,6 +15,6 @@ public sealed class DraftTypeOptions : AbstractOptionGroup
     public override string GroupName => "Draft Type";
     public override uint GroupPriority => 90;
 
-    [ModdedEnumOption("Draft Type", typeof(DraftTypeMode), new[] { "Normal Draft", "Ban + Draft", "Team Captain Battle Royale" })]
+    [ModdedEnumOption("Draft Type", typeof(DraftTypeMode), new[] { "Normal Draft", "Ban + Draft" })]
     public DraftTypeMode DraftType { get; set; } = DraftTypeMode.Normal;
 }
